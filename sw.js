014df501b5811b28acbe4d45fd42e3764620804b
LuisCristovao <36110160+LuisCristovao@users.noreply.github.com>
2021-03-07 13:58:58 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -29,26 +29,13 @@ self.addEventListener('install', function(event) {
     );
   });
   
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-      // caches.match() always resolves
-      // but in case of success response will have value
-      if (response !== undefined) {
-        return response;
-      } else {
-        return fetch(event.request).then(function (response) {
-          // response may be used only once
-          // we need to save clone to put one copy in cache
-          // and serve second one
-          let responseClone = response.clone();
-          
-          caches.open('v1').then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
+  self.addEventListener("fetch", function (event) {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        if (response) {
           return response;
-        }).catch(function () {
-          return caches.match('home.jpg');
-        });
-      }
-    }));
+        }
+        return fetch(event.request);
+      })
+    );
   });
